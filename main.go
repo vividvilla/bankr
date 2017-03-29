ad08cf06742ab0abacac2546d241c3653cc29cfb
Vivek R <vividvilla@gmail.com>
2017-03-30 01:32:32 +0530

    Added config parser and logger

--- main.go
+++ main.go
@@ -2,8 +2,66 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"github.com/spf13/viper"
+	"github.com/spf13/pflag"
+	log "github.com/Sirupsen/logrus"
 )
 
+func init () {
+	// Initialize the app configuration
+	initConfig()
+
+	// Initialize logger
+	initLogger()
+}
+
+// Initializes the app configuration
+func initConfig () {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+
+	// Default configs
+	viper.SetDefault("environment", "debug")
+	// Port to run the app
+	viper.SetDefault("port", ":9000")
+	// Bleve search index path
+	viper.SetDefault("search_index_path", "search.index")
+	// RBI parsed CSV file path
+	viper.SetDefault("data_path", "data.csv")
+	// List of banks in JSON format
+	viper.SetDefault("banks_list_path", "banks.json")
+	// Default bulk insert batch size
+	viper.SetDefault("batch_size", 100)
+	// Reindex on every run
+	viper.SetDefault("re_index", false)
+	// Only index the data instead of starting the server
+	viper.SetDefault("create_index", false)
+	// Banks db path
+	viper.SetDefault("db_path", "banks.db")
+
+	// Parse commandline
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+}
+
+// Initialize loggers
+func initLogger() {
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true, ForceColors: true})
+
+	// Set log level based on environment
+	if viper.GetBool("debug") {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
+}
+
 func main () {
-	fmt.Println("Hello world")
+	log.Debug("Current env : ", viper.GetBool("debug"))
 }
